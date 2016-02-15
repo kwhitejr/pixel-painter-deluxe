bee2fcdf3741e333fd340d7db8ba576865ba9604
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-15 12:01:03 -1000

    installed jade and bodyparser; directory route running

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (req, res) {
+  res.send('index returned');
+});
 
 var server = app.listen(3000, function() {
   console.log('Listening to port', server.address().port);
